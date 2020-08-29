1ddf5a881f1b72d47d86f81a3947f3d123b0d7a8
Jasmin Soltani <jksoltani@gmail.com>
2020-08-29 18:09:55 -0400

    Switch db Pool to db Client

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,10 @@
+/*
 //Create pool for client connection
 const { Pool } = require('pg');
 
 // the line below imports environmental variables from the secrets file
 // but only when we are in development mode
 if (process.env.NODE_ENV !== 'production') require('../../secrets')
-console.log('process.env.DATABASE_URL:',process.env.DATABASE_URL);
 
 const pool = new Pool();
 
@@ -16,3 +16,32 @@ const pool = new Pool();
 module.exports = {
   query: (text, params) => pool.query(text, params)
 }
+*/
+
+
+//Create pool for client connection
+const { Client } = require('pg');
+
+// the line below imports environmental variables from the secrets file
+// but only when we are in development mode
+if (process.env.NODE_ENV !== 'production') require('../../secrets')
+
+const client = new Client();
+client.connect();
+
+// pool.on('error', (err, client) => {
+//   console.error('Unexpected error on idle client', err)
+//   process.exit(-1)
+// })
+
+module.exports = {
+  query: (text, params) => {
+    try {
+      return client.query(text, params);
+    } catch (e) {
+      console.error(e.stack);
+      client.end();
+    }
+  }
+};
+
