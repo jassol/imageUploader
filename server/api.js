f9cccc941ab5c44b1a0b1f74a3d6d4f6dabf93be
Jasmin Soltani <jksoltani@gmail.com>
2020-08-30 10:19:23 -0400

    Log for Heroku Bug2

--- server/api.js
+++ server/api.js
@@ -16,6 +16,7 @@ router.get('/', async (req, res, next) => {
 router.get('/images', async (req, res, next) => {
   try {
     const { rows } = await db.query('SELECT * FROM images WHERE public = true');
+    console.log('rows: ', rows)
     res.send(rows);
   } catch (error) {
     next(error)
